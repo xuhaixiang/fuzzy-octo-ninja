2d12224bb21379e92c7f47358a591b7bdf5aab01
xuhaixiang <xuhaixiang@gmail.com>
2015-01-31 21:53:01 +0800

    add

--- src/main/java/nb/ninja/resources/NinjaResource.java
+++ src/main/java/nb/ninja/resources/NinjaResource.java
@@ -1,10 +1,19 @@
 package nb.ninja.resources;
 
+import redis.clients.jedis.Jedis;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/tasks")
 @Produces(MediaType.APPLICATION_JSON)
 public class NinjaResource {
+
+    @GET
+    public String tasks(@Context Jedis jedis){
+        return jedis.get("task");
+    }
 }
